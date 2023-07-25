aeddecf705a0d7d2abcd3e1105c1abd290353218
Анна Стріхарчук <stannapav@gmail.com>
2023-07-25 14:21:41 +0300

    lil changes to make code more readable

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection.PortableExecutable;
+﻿using System;
 
 namespace homework1
 {
@@ -27,53 +27,57 @@ namespace homework1
             //Task 3
             int size = 3;
             char[] characters = new char[size];
+
+            Console.WriteLine($"Enter {size} characters");
             for(int i = 0; i < size; i++)
             {
-                Console.Write($"Write character {i + 1}: ");
+                Console.Write($"{i + 1} : ");
                 characters[i] = Convert.ToChar( Console.ReadLine() );
             }
 
-            Console.Write("Your charecters are: ");
-            foreach (char charecter in characters)
-                Console.Write($"{charecter} ");
+            Console.Write("Your characters: ");
+            foreach(char character in characters)
+                Console.Write($"{character} ");
+
             Console.WriteLine();
             Console.WriteLine();
 
             //Task 4
-            Console.Write("Enter first number: ");
-            int firstNum = Convert.ToInt32( Console.ReadLine() );
-            Console.Write("Enter second number: ");
+            Console.Write("Enter first number = ");
+            int firstNum = Convert.ToInt32( Console.ReadLine() ) ;
+            Console.Write("Enter second number = ");
             int secondNum = Convert.ToInt32( Console.ReadLine() );
 
             if (firstNum > 0 && secondNum > 0)
                 Console.WriteLine("Both numbers are positive");
             else
                 Console.WriteLine("Both numbers aren't positive");
+
             Console.WriteLine();
 
             //Homework 1
-            Console.Write("Enter lenght of square side: ");
-            int squareSide = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the length of the sides of the square: ");
+            int squareSide = Convert.ToInt32( Console.ReadLine()) ;
 
-            Console.WriteLine($"Area of square {squareSide * squareSide}");
-            Console.WriteLine($"Perimeter of square {4 * squareSide}");
+            Console.WriteLine($"Area of square: {squareSide} * {squareSide} = {squareSide * squareSide}");
+            Console.WriteLine($"Perimeter of square: 4 * {squareSide} = {4 * squareSide}");
             Console.WriteLine();
 
-            //Homework 2 
-            Console.Write("What's your name? ");
+            //Homework 2
+            Console.WriteLine("What's your name?");
             string name = Console.ReadLine();
-            Console.Write($"What's your age, {name}? ");
-            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"What's your age, {name}?");
+            int age = Convert.ToInt32( Console.ReadLine() );
             Console.WriteLine($"{name} is {age} years old");
             Console.WriteLine();
 
             //Homework 3
             Console.Write("Enter radius of circle: ");
-            double r = Convert.ToDouble(Console.ReadLine());
+            double circleRadius = Convert.ToDouble( Console.ReadLine() );
 
-            Console.WriteLine($"Length of circle: 2 * {Math.PI:F2} * {r} = {(2 * Math.PI * r):F2}");
-            Console.WriteLine($"Area of circle: {Math.PI:F2} * {r} * {r} = {(Math.PI * r * r):F2}");
-            Console.WriteLine($"Volume of circle: 4/3 * {Math.PI:F2} * {r} ^ 3 = {( (4/3) * Math.PI * Math.Pow(r, 3) ):F2}");
+            Console.WriteLine($"Length of circle: 2 * {Math.PI:F2} * {circleRadius} = {(2 * Math.PI * circleRadius):F2}");
+            Console.WriteLine($"Area of circle: {Math.PI:F2} * {circleRadius} * {circleRadius} = {(Math.PI * circleRadius * circleRadius):F2}");
+            Console.WriteLine($"Volume of circle: 4/3 * {Math.PI:F2} * {circleRadius}^3 = {( (4/3) * Math.PI * Math.Pow(circleRadius, 3) ):F2}");
         }
     }
 }
