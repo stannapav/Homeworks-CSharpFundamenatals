0eee33bf64c41b29358d683e8426d7527543c474
Анна Стріхарчук <stannapav@gmail.com>
2023-07-28 16:28:45 +0300

    Additional task done

---
+++ Homework2/Hw2_AdidtionalTask/Cat.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw2_AdidtionalTask
+{
+    enum Food { fish = 1, mouse, meat, bird, feed }
+
+    class Cat
+    {
+        string name;
+        int age;
+        int fullness;
+
+        public Cat(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            this.fullness = 0;
+        }
+
+        public void EatSomething(Food something)
+        {
+            switch (something)
+            {
+                case Food.fish:
+                    this.fullness += 5;
+                    Console.WriteLine($"{this.name} ate {Food.fish}");
+                    break;
+
+                case Food.mouse:
+                    this.fullness += 3;
+                    Console.WriteLine($"{this.name} ate {Food.mouse}");
+                    break;
+
+                case Food.meat:
+                    this.fullness += 10;
+                    Console.WriteLine($"{this.name} ate {Food.meat}");
+                    break;
+
+                case Food.bird:
+                    this.fullness += 7;
+                    Console.WriteLine($"{this.name} ate {Food.bird}");
+                    break;
+
+                case Food.feed:
+                    this.fullness += 15;
+                    Console.WriteLine($"{this.name} ate {Food.feed}");
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong option");
+                    break;
+            }
+        }
+
+        public bool checkFullness()
+        {
+            Console.WriteLine((this.fullness >= 30) ? $"{this.name} is full" : $"{this.name} isn't full");
+            return (this.fullness >= 30) ? true : false;
+        }
+    }
+}
---
+++ Homework2/Hw2_AdidtionalTask/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Xml.Linq;
+
+namespace Hw2_AdidtionalTask
+{
+    internal class Program
+    {
+        struct Student
+        {
+            public string lastName;
+            public string groupName;
+
+            public override string ToString()
+            {
+                return $"Students last name is {lastName} from group \"{groupName.Trim().ToLower()}\"";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Cat
+            Cat myCat = new Cat("Diana", 12);
+
+            bool fullOrNot = false;
+            do
+            {
+                Console.WriteLine("Feed your cat.");
+                Console.WriteLine("1: Fish");
+                Console.WriteLine("2: Mouse");
+                Console.WriteLine("3: Meat");
+                Console.WriteLine("4: Bird");
+                Console.WriteLine("5: Feed");
+                Console.Write("Choose what you will give it: ");
+
+                int? readReasult = Convert.ToInt32(Console.ReadLine());
+                Food choice = (readReasult != null) ? (Food)readReasult : (Food)0;
+                Console.WriteLine();
+
+                myCat.EatSomething(choice);
+                fullOrNot = myCat.checkFullness();
+                Console.WriteLine();
+            } while (!fullOrNot);
+
+            //Student
+            int sizeOfArray = 3;
+            Student[] students = new Student[sizeOfArray];
+
+            for(int i = 0; i < sizeOfArray; i++)
+            {
+                Console.Write($"Enter {i + 1} student last name: ");
+                students[i].lastName = Console.ReadLine();
+                Console.Write("and group name: ");
+                students[i].groupName = Console.ReadLine();
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter group name: ");
+            string groupName = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine($"List of student in group \"{groupName.Trim().ToLower()}\"");
+            for (int i = 0; i < sizeOfArray; i++)
+            {
+                Console.Write( (groupName.Trim().ToLower() == students[i].groupName.Trim().ToLower() ) ? $"{i + 1}: " + students[i] + "\n" : "");
+            }
+        }
+    }
+}
