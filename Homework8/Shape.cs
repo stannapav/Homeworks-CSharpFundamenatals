8cf6f9d670e91fbbdaa77e97269380fdf44fad4a
Anna Striharchuk <116493283+stannapav@users.noreply.github.com>
2023-08-21 08:40:38 +0300

    Homework8 done

---
+++ Homework8/Shape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8
+{
+    abstract class Shape : IComparable<Shape>
+    {
+        string name;
+
+        public string Name { get { return name; } }
+
+        public Shape(string name)
+        {
+            this.name = name;
+        }
+
+        abstract public double Area();
+        abstract public double Perimetr();
+        public abstract int CompareTo(Shape other);
+    }
+}
